a623d56f0d145f3c2454402bf6d849fd54fed734
Sandeep <rj91779@umbc.edu>
2024-04-04 20:09:44 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-    content: ['./src/**/*.{html,js}',
-    "./node_modules/flowbite/**/*.js"],
-    theme:{
-        extend:{
-            
+    content: ["./src/**/*.{html,js}"],
+    darkMode: 'class',
+    theme: {
+      extend: {
         screens: {
-            "other": {'min': '340px', 'max': '1200px'},
-
+          "other": {'min': '340px', 'max': '1200px'},
         },
+        colors: {
+          darkbg: "#1E293B",
+          blue: {
+            850: "#1e40af"
+          }
+        }
+      },
     },
-    plugins:[require('flowbite/plugin')],
-}
+    plugins: [],
 }
